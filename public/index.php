79a1f4ea57012cdab179ba852cf2a71196f7cd1b
viniciussantosm <vinicius.santos@bis2bis.com.br>
2023-07-10 10:52:58 -0300

    Changed router configs to new Router Class

--- public/index.php
+++ public/index.php
@@ -1,20 +1,9 @@
 <?php
 
 require("vendor/autoload.php");
-require_once __DIR__ . "/../config/router.php";
 
-use src\Controller\{
-    PostController,
-    UserController,
-};
+use src\Router\Router;
 
-route("/posts", PostController::class);
-route("/users", UserController::class);
-
-$action = $_SERVER["REQUEST_URI"];
-
-if($action == "/"){
-    header("Location: /posts/index");
-}
-$method = $_SERVER["REQUEST_METHOD"];
-dispatch($action, $method);
+$router = new Router();
+$router->init();
+$router->dispatch();
