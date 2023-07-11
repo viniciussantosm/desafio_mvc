72ce377b30bb602b964c21b273d56dff29506a3c
viniciussantosm <vinicius.santos@bis2bis.com.br>
2023-07-11 09:00:54 -0300

    Controllers created for each model

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -5,9 +5,23 @@ namespace src\Controller;
 
 abstract class Controller {
 
+    private $notFoundView = "404";
+
     public function view($viewName)
     {
+        $this->validateViewName($viewName);
         $requiredViewName = str_replace(".", "/", $viewName);
-        return require_once(__DIR__ . "/../../views/$requiredViewName.php");
+        return [
+            require_once(__DIR__ . "/../../views/header.php"),
+            require_once(__DIR__ . "/../../views/$requiredViewName.php"),
+            require_once(__DIR__ . "/../../views/footer.php"),
+        ];
+    }
+
+    public function validateViewName($viewName)
+    {
+        if(!is_string($viewName)) {
+            return require_once(__DIR__ . "/../../views/$this->notFoundView.php");
+        }
     }
 }
