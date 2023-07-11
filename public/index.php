45e2ebca4a6bd69ca6a6c01f5bcb5d5178557358
viniciussantosm <vinicius.santos@bis2bis.com.br>
2023-07-11 09:01:54 -0300

    Router implemented

--- public/index.php
+++ public/index.php
@@ -1,8 +1,16 @@
 <?php
 
 require("vendor/autoload.php");
+require("config/config.ini.php");
+require("config/Conn.php");
 
 use src\Router\Router;
+use config\Database;
+
+define("ROOT", __DIR__);
+
+$conn = new Database(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
+$link = $conn->connect();
 
 $router = new Router();
 $router->init();
