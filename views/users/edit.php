48529f0095a6ed3db49d494f50b437c19ba2930b
viniciussantosm <vinicius.santos@bis2bis.com.br>
2023-07-12 15:41:56 -0300

    Created edit user's information view

---
+++ views/users/edit.php
@@ -0,0 +1,27 @@
+<div class="container">
+    <div class="title-container">
+        <h1>Editar Dados</h1>
+    </div>
+    <div class="login-form-container">
+        <form class="login-form" action="http://<?=$_SERVER["HTTP_HOST"]?>/users/update" method="post">
+            <input type="hidden" value="<?=$data["id"]?>" name="id">
+            <div class="form-control">
+                <label for="name">Nome:</label>
+                <input type="name" name="name" value="<?=$data["name"]?>" required>
+            </div>
+            <div class="form-control">
+                <label for="email">Email:</label>
+                <input type="email" name="email" value="<?=$data["email"]?>" readonly required>
+            </div>
+            <div class="form-control">
+                <label for="password">Senha:</label>
+                <input type="password" name="password" required>
+            </div>
+            <div class="form-control">
+                <label for="password">Confirme a senha:</label>
+                <input type="password" name="passwordConfirm" required>
+            </div>
+            <button class="btn" type="submit">Login</button>
+        </form>
+    </div>
+</div>
