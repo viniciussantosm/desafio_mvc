e9651776b8128fc120322283004f5fc436a9af6d
viniciussantosm <vinicius.santos@bis2bis.com.br>
2023-07-12 15:39:06 -0300

    Added flash message container

--- views/header.php
+++ views/header.php
@@ -15,7 +15,22 @@
             <li><a href="http://<?=$_SERVER["HTTP_HOST"]?>/posts/index">Home</a></li>
             <li><a href="http://<?=$_SERVER["HTTP_HOST"]?>/categories/index">Categorias</a></li>
             <li><a href="http://<?=$_SERVER["HTTP_HOST"]?>/tags/index">Tags</a></li>
-            <li><a href="http://<?=$_SERVER["HTTP_HOST"]?>/posts/index">Meus Posts</a></li>
-            <li><a href="http://<?=$_SERVER["HTTP_HOST"]?>/auth/login">Login</a></li>
+            <?php if(isset($_SESSION["isLoggedIn"])): ?>
+                <li><a href="http://<?=$_SERVER["HTTP_HOST"]?>/users/index">Meus Posts</a></li>
+                <li><span>Bem-vindo,</span> <a href="http://<?=$_SERVER["HTTP_HOST"]?>/users/edit/<?=$_SESSION["userId"]?>"><?=$_SESSION["name"]?></a> / <a href="http://<?=$_SERVER["HTTP_HOST"]?>/auth/logout">Sair</a></li>
+            <?php else: ?>
+                <li><a href="http://<?=$_SERVER["HTTP_HOST"]?>/auth/login">Login</a></li>
+                <li><a href="http://<?=$_SERVER["HTTP_HOST"]?>/auth/register">Registrar</a></li>
+            <?php endif; ?>
         </ul>
     </nav>
+    <?php if(isset($_SESSION["message"])): ?>
+    <div class="flash-message-container">
+        <div class="flash-message <?=$_SESSION["message"]["type"]?>">
+            <p><?php
+                echo $_SESSION["message"]["value"];
+                unset($_SESSION["message"]);
+            ?></p>
+        </div>
+    </div>
+    <?php endif; ?>
