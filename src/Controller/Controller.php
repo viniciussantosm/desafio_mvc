eeadebaf4157de394c503e63adcfa94f324e2a5b
viniciussantosm <vinicius.santos@bis2bis.com.br>
2023-07-12 15:33:31 -0300

    Added possibility to send data to the view

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -7,7 +7,7 @@ abstract class Controller {
 
     private $notFoundView = "404";
 
-    public function view($viewName)
+    public function view($viewName, $data = [])
     {
         $this->validateViewName($viewName);
         $requiredViewName = str_replace(".", "/", $viewName);
@@ -15,6 +15,7 @@ abstract class Controller {
             require_once(__DIR__ . "/../../views/header.php"),
             require_once(__DIR__ . "/../../views/$requiredViewName.php"),
             require_once(__DIR__ . "/../../views/footer.php"),
+            $data,
         ];
     }
 
