1fd363b3eb5c1a7d90c953efc72f558a7a0ebc03
viniciussantosm <vinicius.santos@bis2bis.com.br>
2023-07-12 15:40:54 -0300

    Created new User view

---
+++ views/auth/create.php
@@ -0,0 +1,26 @@
+<div class="container">
+    <div class="title-container">
+        <h1>Create</h1>
+    </div>
+    <div class="login-form-container">
+        <form class="login-form" action="http://<?=$_SERVER["HTTP_HOST"]?>/auth/store" method="post">
+            <div class="form-control">
+                <label for="name">Nome:</label>
+                <input type="name" name="name" required value="<?=$data["name"]?>">
+            </div>
+            <div class="form-control">
+                <label for="email">Email:</label>
+                <input type="email" name="email" required value="<?=$data["email"]?>">
+            </div>
+            <div class="form-control">
+                <label for="password">Senha:</label>
+                <input type="password" name="password" required>
+            </div>
+            <div class="form-control">
+                <label for="password">Confirme a senha:</label>
+                <input type="password" name="passwordConfirm" required>
+            </div>
+            <button class="btn" type="submit">Login</button>
+        </form>
+    </div>
+</div>
