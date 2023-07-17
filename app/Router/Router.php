e93d9bd4b3ebd278605d6906b1d896945cad94f7
viniciussantosm <vinicius.santos@bis2bis.com.br>
2023-07-17 14:51:02 -0300

    Router improvements

--- app/Router/Router.php
+++ app/Router/Router.php
@@ -5,76 +5,51 @@ namespace App\Router;
 use App\Controller\{
     AuthController,
     CategoryController,
+    Controller,
     PostController,
     UserController,
     TagController,
 };
-use App\Model\Messages\Messages;
-use App\Model\Session;
+use App\Model\Command\Route;
 
 class Router {
 
-    private $routes = [];
-    private $validControllerMethods = ["index", "create", "store", "edit", "update", "destroy", "login", "register"];
-    private Messages $routerMessager;
-    private Messages $userMessager;
-    private Session $session;
+    private array $routes = [];
+    
+    protected $uri;
 
-    public function __construct(Messages $messager, Messages $userMessager, Session $session) {
-        $this->routerMessager = $messager;
-        $this->userMessager = $userMessager;
-        $this->session = $session;
-    }
 
-    function init() {
-        
-        $this->routes = [
-            "posts" => PostController::class,
-            "users" => UserController::class,
-            "categories" => CategoryController::class,
-            "tags" => TagController::class,
-            "auth" => AuthController::class,
-        ];
-    }
-
-    public function dispatch() {
-
-        if(!$this->validateUri($_SERVER["REQUEST_URI"], $this->routerMessager)) return false;
-        
-        $action = trim($_SERVER["REQUEST_URI"], '/');
-        $action = explode("/", $action);
-        
-        $className = (string)$this->routes[$action[0]];
-
-        if(!$className) {
-            exit();
+    public function addRoute(string $route, string $controller) {
+        $route = new Route($route, $controller, $this);
+        if (sizeof($this->routes) > 0) {
+            $route->setNext(
+                array_pop($this->routes)
+            );
         }
-
-        $controller = new $className(
-                            $this->routerMessager,
-                            $this->userMessager,
-                            $this->session
-                        );
-        $controller->{$action[1]}();
+        $this->routes[] = $route;
     }
 
-    public function validateUri($uri, Messages $message)
+    public function handle() {
+        /**
+         * @var Route $firstRoute
+         */
+        $firstRoute = array_shift($this->routes);
+
+        return $firstRoute->handle($_SERVER["REQUEST_URI"] ?: '/');
+    }
+
+    public function dispatch(Controller $controller, $action) {
+
+        $controller->$action();
+    }
+
+    public function validateUri($uri)
     {
         if(!is_string($uri) || strlen($uri) <= 1) {
-            $this->redirect($message->getMessage("posts.index"));
+            Router::redirect("posts/index");
         }
-        
-        if($this->checkForStaticFile(ROOT . $uri)) return false;
 
         $explodedUri = explode("/", trim($uri, "/"));
-
-        if(!array_key_exists($explodedUri[0], $this->routes)) {
-            $this->redirect($message->getMessage("posts.index"));
-        }
-
-        if(array_key_exists(1 ,$explodedUri) && !in_array($explodedUri[1], $this->validControllerMethods)) {
-            $this->redirect($message->getMessage("posts.index"));
-        }
 
         if(array_key_exists(1 ,$explodedUri) && $explodedUri[1] == "edit") {
             $this->validateEditUri($explodedUri);
@@ -83,39 +58,11 @@ class Router {
         return true;
     }
 
-    public function checkForStaticFile($filePath)
-    {
-        if (file_exists($filePath)) {
-            $this->setContentType($filePath);
-            $this->showFileContent($filePath);
-            
-            return true;
-        }
-    }
-
-    public function showFileContent($filePath)
-    {
-        $fh = fopen($filePath, 'r');
-        fpassthru($fh);
-        fclose($fh);
-    }
-
-    public function setContentType($filePath)
-    {
-        if(strpos($filePath, ".css")) {
-            header('Content-Type: text/css');
-            return true;
-        }
-
-        header('Content-Type: '.mime_content_type($filePath));
-        return true;
-    }
-
     public function validateEditUri(array $explodedUri)
     {
         $value = filter_var($explodedUri[2], FILTER_SANITIZE_NUMBER_INT);
         if(!$value) {
-            self::redirect("/posts/index");
+            Router::redirect("/posts/index");
         }
     }
 
