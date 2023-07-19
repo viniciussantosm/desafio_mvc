a108a4ca1f15c25e403ebf4d99d522a003b10828
viniciussantosm <vinicius.santos@bis2bis.com.br>
2023-07-19 08:41:28 -0300

    Inclusion of new routes

--- public/index.php
+++ public/index.php
@@ -1,11 +1,9 @@
 <?php
-use App\Controller\Auth\LoginController;
-use App\Controller\Auth\LogoutController;
-use App\Controller\Auth\RegisterController;
-use App\Controller\Auth\StoreController;
+use App\Controller\Posts\CreateController;
+use App\Controller\Posts\EditController;
 use App\Controller\Posts\IndexController;
-use App\Controller\Users\EditController;
-use App\Controller\Users\UpdateController;
+use App\Controller\Posts\StoreController;
+use App\Controller\Posts\UpdateController;
 
 
 ini_set("display_errors", 1);
@@ -22,15 +20,19 @@ $router->addRoute("/", IndexController::class);
 // Posts Routes
 $router->addRoute("/posts", IndexController::class);
 $router->addRoute("/posts/index", IndexController::class);
+$router->addRoute("/posts/create", CreateController::class);
+$router->addRoute("/posts/store", StoreController::class);
+$router->addRoute("/posts/edit", EditController::class);
+$router->addRoute("/posts/update", UpdateController::class);
 //Auth Routes
 $router->addRoute("/auth/index", \App\Controller\Auth\IndexController::class);
-$router->addRoute("/auth/login", LoginController::class);
-$router->addRoute("/auth/register", RegisterController::class);
-$router->addRoute("/auth/store", StoreController::class);
-$router->addRoute("/auth/logout", LogoutController::class);
+$router->addRoute("/auth/login", \App\Controller\Auth\LoginController::class);
+$router->addRoute("/auth/register", \App\Controller\Auth\RegisterController::class);
+$router->addRoute("/auth/store", \App\Controller\Auth\StoreController::class);
+$router->addRoute("/auth/logout", \App\Controller\Auth\LogoutController::class);
 //Users Routes
-$router->addRoute("/users/edit", EditController::class);
-$router->addRoute("/users/update", UpdateController::class);
+$router->addRoute("/users/edit", \App\Controller\Users\EditController::class);
+$router->addRoute("/users/update", \App\Controller\Users\UpdateController::class);
 $router->addRoute("/users/index", \App\Controller\Users\IndexController::class);
 //Categories Routes
 $router->addRoute("/categories/index", \App\Controller\Categories\IndexController::class);
@@ -38,5 +40,8 @@ $router->addRoute("/categories/create", \App\Controller\Categories\CreateControl
 $router->addRoute("/categories/store", \App\Controller\Categories\StoreController::class);
 //Tags Routes
 $router->addRoute("/tags/index", \App\Controller\Tags\IndexController::class);
+$router->addRoute("/tags/create", \App\Controller\Tags\CreateController::class);
+$router->addRoute("/tags/store", \App\Controller\Tags\StoreController::class);
+
 $router->handle();
 return;
