5dcdb2be78ef35dbab4c99cdc68932cc82265931
viniciussantosm <vinicius.santos@bis2bis.com.br>
2023-07-19 16:51:20 -0300

    New routes added

--- public/index.php
+++ public/index.php
@@ -1,7 +1,9 @@
 <?php
 use App\Controller\Posts\CreateController;
+use App\Controller\Posts\DestroyController;
 use App\Controller\Posts\EditController;
 use App\Controller\Posts\IndexController;
+use App\Controller\Posts\ShowController;
 use App\Controller\Posts\StoreController;
 use App\Controller\Posts\UpdateController;
 
@@ -24,6 +26,8 @@ $router->addRoute("/posts/create", CreateController::class);
 $router->addRoute("/posts/store", StoreController::class);
 $router->addRoute("/posts/edit", EditController::class);
 $router->addRoute("/posts/update", UpdateController::class);
+$router->addRoute("/posts/destroy", DestroyController::class);
+$router->addRoute("/posts/show", ShowController::class);
 //Auth Routes
 $router->addRoute("/auth/index", \App\Controller\Auth\IndexController::class);
 $router->addRoute("/auth/login", \App\Controller\Auth\LoginController::class);
@@ -38,10 +42,24 @@ $router->addRoute("/users/index", \App\Controller\Users\IndexController::class);
 $router->addRoute("/categories/index", \App\Controller\Categories\IndexController::class);
 $router->addRoute("/categories/create", \App\Controller\Categories\CreateController::class);
 $router->addRoute("/categories/store", \App\Controller\Categories\StoreController::class);
+$router->addRoute("/categories/edit", \App\Controller\Categories\EditController::class);
+$router->addRoute("/categories/update", \App\Controller\Categories\UpdateController::class);
+$router->addRoute("/categories/destroy", \App\Controller\Categories\DestroyController::class);
+$router->addRoute("/categories/show", \App\Controller\Categories\ShowController::class);
 //Tags Routes
 $router->addRoute("/tags/index", \App\Controller\Tags\IndexController::class);
 $router->addRoute("/tags/create", \App\Controller\Tags\CreateController::class);
 $router->addRoute("/tags/store", \App\Controller\Tags\StoreController::class);
+$router->addRoute("/tags/edit", \App\Controller\Tags\EditController::class);
+$router->addRoute("/tags/update", \App\Controller\Tags\UpdateController::class);
+$router->addRoute("/tags/destroy", \App\Controller\Tags\DestroyController::class);
+$router->addRoute("/tags/show", \App\Controller\Tags\ShowController::class);
+// Dashboard Rotues
+$router->addRoute("/dashboard/index", \App\Controller\Dashboard\IndexController::class);
+$router->addRoute("/dashboard/categories", \App\Controller\Dashboard\CategoriesDashController::class);
+$router->addRoute("/dashboard/tags", \App\Controller\Dashboard\TagsDashController::class);
+// Comment Routes
+$router->addRoute("/comments/store", \App\Controller\Comments\StoreController::class);
 
 $router->handle();
 return;
