a0fc324a49eaa788eff59aded31bcd91f0853fe8
viniciussantosm <vinicius.santos@bis2bis.com.br>
2023-07-20 17:11:49 -0300

    Added new server constant

--- config/config.ini.php
+++ config/config.ini.php
@@ -1,7 +1,9 @@
 <?php
-
+//Database constants
 define("DB_DRIVER", "mysql");
 define("DB_SERVER", "localhost");
 define("DB_USER", "admin");
 define("DB_PASSWORD", "admin123");
 define("DB_NAME", "bloggero");
+//System constants
+define("HTTP_HOST", $_SERVER["HTTP_HOST"]);
